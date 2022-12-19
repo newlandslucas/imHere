d0108cd7fbccc79e2fcd18dcf24b03d7666a9f4d
C1326386 Lucas Silveira Newlands Machado <lucas.newlands@ibm.com>
2022-12-19 01:53:26 -0300

    Start project

--- App.tsx
+++ App.tsx
@@ -1,20 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Text, SafeAreaView } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return(
+    <>
+      <SafeAreaView style={{
+        flex: 1,
+        backgroundColor: '#252525',
+        
+      }}>
+        <Text style={{color: '#FFF', fontSize: 24, fontWeight: 'bold', padding: 24}}>React</Text>
+        <Text>Native</Text>
+      </SafeAreaView>
+    </>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
